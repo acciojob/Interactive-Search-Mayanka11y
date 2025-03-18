a25dc53dfa075c308cabda0061af1d42f782f65e
Mayanka11y <mayankraj418890@gmail.com>
2025-03-18 12:01:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+// Get references to the search components
+const searchDiv = document.querySelector(".search");
+const searchInput = document.querySelector(".input");
+const searchButton = document.querySelector(".btn");
+
+// Add a click event listener to the button
+searchButton.addEventListener("click", () => {
+  // Add the 'active' class to the search div
+  searchDiv.classList.add("active");
+
+  // Focus the input field
+  searchInput.focus();
+});
